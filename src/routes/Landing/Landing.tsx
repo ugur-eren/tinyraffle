025d493832cb45dc8c7d15b7a43a815298e24080
Uğur Eren <contact@ugureren.net>
2023-01-15 02:22:58 +0300

    Divider & grid structure for Landing

--- src/routes/Landing/Landing.tsx
+++ src/routes/Landing/Landing.tsx
@@ -7,31 +7,39 @@ const Landing: React.FC = () => {
   return (
     <div className="p-landing">
       <div className="p-landing__content w-limited">
-        <StepCard index={1} title="Login With Sol Wallet">
-          <FormElement>
-            <FormElement.Button>Connect</FormElement.Button>
-          </FormElement>
-        </StepCard>
+        <div className="p-landing__left">
+          <StepCard index={1} title="Login With Sol Wallet">
+            <FormElement>
+              <FormElement.Button>Connect</FormElement.Button>
+            </FormElement>
+          </StepCard>
 
-        <StepCard
-          index={2}
-          title="Upload Your List (CSV Format)"
-          description="Enter How Many lines in the raffle?"
-        >
-          <FormElement>
-            <button type="button">Upload</button>
-          </FormElement>
+          <StepCard
+            index={2}
+            title="Upload Your List (CSV Format)"
+            description="Enter How Many lines in the raffle?"
+          >
+            <FormElement>
+              <button type="button">Upload</button>
+            </FormElement>
 
-          <FormElement>
-            <FormElement.Input type="text" />
-          </FormElement>
-        </StepCard>
+            <FormElement>
+              <FormElement.Input type="text" />
+            </FormElement>
+          </StepCard>
 
-        <StepCard index={3} title="See the Winner">
-          <FormElement>
-            <button type="button">Show Winner</button>
-          </FormElement>
-        </StepCard>
+          <StepCard index={3} title="See the Winner">
+            <FormElement>
+              <button type="button">Show Winner</button>
+            </FormElement>
+          </StepCard>
+        </div>
+
+        <div className="p-landing__divider" />
+
+        <div className="p-landing__right">
+          <h1>Mini Raffle</h1>
+        </div>
       </div>
     </div>
   );
